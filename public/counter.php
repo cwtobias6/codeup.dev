673086c5cb2ee2a269d9f9e9c06a3b326874d7e6
Christian Tobias <cwtobias6@gmail.com>
2015-07-22 15:41:00 -0500

    finished counter game

--- public/counter.php
+++ public/counter.php
@@ -1,7 +1,13 @@
 <?php 
 
-$counter = empty($_GET['count'] ? $_GET['count'] : '');
+var_dump($_GET);
+$counter = isset($_GET['counter']) ? $_GET['counter'] : 0;
 
+if(!empty($_GET["up"])) {
+	$counter++;
+} else if(!empty($_GET["down"])){
+	$counter--;
+}
 
 
 
@@ -13,8 +19,8 @@ $counter = empty($_GET['count'] ? $_GET['count'] : '');
 	<title>Counter Exercise</title>
 </head>
 <body>
-	<h2>Counter Value <?= $counter ?></h2>
-	<a href="?counter=<?= $counter ?>&&up=true">Going Up</a>
-	<a href="?counter=<?= $counter ?>&&down=true">Going Down</a>
+	<h2>Counter Value: <?= $counter ?></h2>
+	<a href="?counter=<?= $counter ?>&up=true">Going Up</a>
+	<a href="?counter=<?= $counter ?>&down=true">Going Down</a>
 </body>
 </html>
